a33e380b188c24bbcca3ed7ffbc14c607e97d418
Mikhail Abramishvili <mg@abramishvili.net>
2022-11-17 20:33:25 +0400

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            ShopSeeder::class,
+            // ShopSeeder::class,
         ]);
     }
 }
