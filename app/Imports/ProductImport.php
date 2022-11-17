61cb6e5395e53e5526a08a1388716c288c5362ba
Mikhail Abramishvili <mg@abramishvili.net>
2022-11-17 17:22:59 +0400

    .

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -12,16 +12,19 @@ class ProductImport implements ToModel, WithStartRow
     public function model(array $row)
     {
         $request = request()->all();
-
-        return new Product([
-            'manufacturer' => rtrim($row[0]),
-            'artnumber' => rtrim($row[1]),
-            'name' => rtrim($row[2]),
-            'price' => rtrim($row[3]),
-            'balance' => rtrim($row[4]),
-            'category_id' => 1,
-            'pricelist_id' => 1,
-        ]);
+        
+        if(rtrim($row[3]) > 0 && rtrim($row[4]) > 0)
+        {
+            return new Product([
+                'manufacturer' => rtrim($row[0]),
+                'artnumber' => rtrim($row[1]),
+                'name' => rtrim($row[2]),
+                'price' => rtrim($row[3]),
+                'balance' => rtrim($row[4]),
+                'category_id' => 1,
+                'pricelist_id' => 1,
+            ]);
+        }
     }
 
     public function startRow(): int
