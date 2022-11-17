44ca5e5e31ee280440df497577fbe8a691863df8
Mikhail Abramishvili <mg@abramishvili.net>
2022-11-17 17:26:54 +0400

    .

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -18,7 +18,7 @@ class ProductImport implements ToModel, WithStartRow
             'artnumber' => rtrim($row[1]),
             'name' => rtrim($row[2]),
             'price' => rtrim($row[3]),
-            'balance' => rtrim($row[4]),
+            'balance' => $row[4] ? rtrim($row[4]) : NULL,
             'category_id' => 1,
             'pricelist_id' => 1,
         ]);
