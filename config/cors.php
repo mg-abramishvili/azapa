625864b9574e3b4fb96a657cbc35fd75672a4980
Mikhail Abramishvili <mg@abramishvili.net>
2022-11-17 20:03:57 +0400

    .

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.zapchasti-kia-rio.ru'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
